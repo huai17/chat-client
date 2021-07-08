3f4fa6bd97000eca4da66a41098e56cc75836d97
Huai <shih820517@gmail.com>
2021-07-08 22:54:39 +0800

    setup App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { CssBaseline, CircularProgress } from "@material-ui/core";
+import { JoinRoomForm } from "./components/JoinRoomForm";
+import { ChatRoom } from "./components/ChatRoom";
+import { useChatService } from "./service/chat-service";
+
 const App = () => {
-  return <div>Hello</div>;
+  const { join, error, ...chat } = useChatService();
+
+  return (
+    <>
+      <CssBaseline />
+      {chat.status === "joined" ? (
+        <ChatRoom {...chat} />
+      ) : chat.status === "connected" ? (
+        <JoinRoomForm error={error} join={join} />
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  );
 };
 
 export default App;
